c43fb57dbea3133165ba9c4ef3a50aafe76fdb4a
Veri5ied <43749581+Veri5ied@users.noreply.github.com>
2020-09-09 04:30:18 +0100

    book summary and stylings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BookContextProvider from './contexts/BookContext';
 import Navbar from './components/navbar/Navbar';
+import BookList from './components/book/BookList';
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar />
+        <BookList />
       </BookContextProvider>
     </div>
   );
