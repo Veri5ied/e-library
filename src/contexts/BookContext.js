fb0cc6745170fa36ece0936b3844070e5034585f
Veri5ied <43749581+Veri5ied@users.noreply.github.com>
2020-09-09 21:29:51 +0100

    loacl storage

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,8 +1,11 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const BookContext = createContext();
 const BookContextProvider = (props) => {
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([], () => {
+    const localData = localStorage.getItem("books");
+    return localData ? JSON.parse(localData) : [];
+  });
 
   const addBook = (title, author) => {
     setBooks([...books, { title: title, author: author, id: Math.random() }]);
@@ -11,6 +14,11 @@ const BookContextProvider = (props) => {
   const removeBook = (id) => {
     setBooks(books.filter((book) => book.id !== id));
   };
+
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
+
   return (
     <BookContext.Provider value={{ books, addBook, removeBook }}>
       {props.children}
