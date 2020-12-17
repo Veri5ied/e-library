bc7410475544a1bc328ebb2073dac85850419985
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2020-12-17 14:16:33 +0100

    context and useReducer sync

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,26 +1,12 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useReducer } from "react";
+import { bookReducer } from "../reducers/bookReducer";
 
 export const BookContext = createContext();
 const BookContextProvider = (props) => {
-  const [books, setBooks] = useState([], () => {
-    const localData = localStorage.getItem("books");
-    return localData ? JSON.parse(localData) : [];
-  });
-
-  const addBook = (title, author) => {
-    setBooks([...books, { title: title, author: author, id: Math.random() }]);
-  };
-
-  const removeBook = (id) => {
-    setBooks(books.filter((book) => book.id !== id));
-  };
-//setting all inputs to localstorage
-  useEffect(() => {
-    localStorage.setItem("books", JSON.stringify(books));
-  }, [books]);
+  const [books, dispatch] = useReducer(bookReducer, []);
 
   return (
-    <BookContext.Provider value={{ books, addBook, removeBook }}>
+    <BookContext.Provider value={{ books, dispatch }}>
       {props.children}
     </BookContext.Provider>
   );
